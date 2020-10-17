7870d8b5c22211292070436f4c9ae35f18fe8da6
Ernesto Orellana <ernestoorellana5267@gmail.com>
2020-10-17 12:39:04 -0600

    Interfaz Arreglada

--- app/src/main/java/com/example/tacobell/MainActivity.java
+++ app/src/main/java/com/example/tacobell/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
         ConexionSQLite conn=new ConexionSQLite(this,"bd_usuarios",null,1);
     }
+
+
 }
--- app/src/main/java/com/example/tacobell/RegistroUsuarioActivity.java
+++ app/src/main/java/com/example/tacobell/RegistroUsuarioActivity.java
@@ -1,9 +1,16 @@
 package com.example.tacobell;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.strictmode.SqliteObjectLeakedViolation;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tacobell.utilidades.Utilidades;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
 
@@ -14,7 +21,30 @@ public class RegistroUsuarioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        campoDPI = (EditText) findViewById(R.id.campoID);
+        campoID = (EditText) findViewById(R.id.campoID);
+        campoDPI = (EditText) findViewById(R.id.campoDPI);
+        campoNombre = (EditText) findViewById(R.id.campoNombre);
 
+    }
+    public void onClick(View view){
+        registrarUsuarios();
+    }
+
+    private void registrarUsuarios() {
+        ConexionSQLite conn=new ConexionSQLite(this,"bd_usuarios",null,1);
+
+        SQLiteDatabase db = conn.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(Utilidades.CAMPO_ID,campoID.getText().toString());
+        values.put(Utilidades.NOMBRE,campoNombre.getText().toString());
+        values.put(Utilidades.DPI,campoDPI.getText().toString());
+
+        Long idResultante = db.insert(Utilidades.TABLA_USUARIO, Utilidades.CAMPO_ID,values);
+
+        Toast.makeText(getApplicationContext(),"ID Registro"+ idResultante, Toast.LENGTH_SHORT).show();
+
+        db.close();
     }
 }
